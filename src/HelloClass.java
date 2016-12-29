557c15cffa0e32fbf8b9a64b51bba0215e8e40ab
Etyamor <maxikrud0071@gmail.com>
2016-12-29 14:24:53 +0200

    bla bla

---
+++ src/HelloClass.java
@@ -0,0 +1,11 @@
+/**
+ * Created by tmp on 29.12.2016.
+ */
+public class HelloClass {
+    public static void main(String[] args) {
+        System.out.print("Hello WORLD!");
+        //first project on GIT
+    }
+}
+
+
