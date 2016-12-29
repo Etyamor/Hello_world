16aef838755fc915a5f0a129dddaaa8a8e543307
Etyamor <maxikrud0071@gmail.com>
2016-12-29 14:47:47 +0200

    bla bla

--- src/HelloClass.java
+++ src/HelloClass.java
@@ -8,4 +8,19 @@ public class HelloClass {
     }
 }
 
-
+ class BubbleSort{
+     public static void main(String[] args) {
+         int n = 5;
+         int[] a = new int[5];
+         a[0]=5;a[1]=10122;a[2]=-42;a[3]=4;a[4]=2;
+         for (int i = 0; i < n; i++)
+             for (int j = 0; j < n - 1; j++)
+                 if (a[j] > a[j + 1]) {
+                    int sw = a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=sw;
+                 };
+         for (int i = 0; i < n; i++)
+             System.out.println(a[i]);
+     }
+ }
